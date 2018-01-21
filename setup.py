f0a4cead342989aaec06da1f6c00fc40a3373e44
Kit Peterson <kpeterstech@gmail.com>
2018-01-21 16:09:53 -0600

    incorporated setuptools
    originally dupfileremover was just a single python file. Setuptools
    has now been incorporated to make adding additional features and
    managing the project easier as time goes on

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dupfileremover',
+      version='0.1',
+      description='A command line utility that helps you find duplicate files',
+      url='http://github.com/kpeterstech/dupfileremover',
+      author='Kit Peterson',
+      author_email='kpeterstech@gmail.com',
+      license='MIT',
+      packages=[],
+      zip_safe=False)
