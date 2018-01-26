0a810d1ea104c719c0eabd1c9fc9e780399b656c
Kit Peterson <kpeterstech@gmail.com>
2018-01-25 18:05:03 -0600

    added MANIFEST.in README.rst and added long desc in setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 from setuptools import setup
 
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
 setup(name='dupfileremover',
       version='0.2',
       description='A command line utility that helps you find  and remove duplicate files',
+      long_description=readme(),
       url='http://github.com/kpeterstech/dupfileremover',
       author='Kit Peterson',
       author_email='kpeterstech@gmail.com',
